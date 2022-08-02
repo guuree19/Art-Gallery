26af55d909fa6ba17329e7b0be8af793025f5951
Abdulaziz Ali <guuree1982@gmail.com>
2022-08-01 21:28:43 -0400

    art gallery app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react'
+import Gallery from './Gallery.js'
+import ButtonBar from './ButtonBar.js'
 
-function App() {
+
+function App( ){
+  let [artId, setArtId] = useState(12720)
+  let [data, setData] = useState({})
+  const handleIterate = (e) => {
+    setArtId(artId + Number(e.target.value))
+  }
+
+
+  useEffect(() => {
+      document.title='Welcome to Artworld'
+      fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${artId}`)
+      .then(response => response.json())
+      .then(resData => setData(resData))
+  }, [artId])
+  const imageDisplay = () => {
+    if (!data.primaryImage) {
+      return (
+        <h2>No image found</h2>
+      )
+    }
+    return (
+      <Gallery objectImg={data.primaryImage} artist={data.artistDisplayName} title={data.title} />
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{imageDisplay()}</div>
+      <ButtonBar handleIterate={handleIterate} />
     </div>
   );
 }
+
+
+
 
 export default App;
